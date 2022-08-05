a84d85e8203e55f45e89addc1323fc69dd4b3d6d
abedinforhane <farhanengn@gmail.comm>
2022-08-05 17:48:20 +0600

    put and post functionality completed

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express=require('express');
-const mongoose  = require('mongoose');
-const todoHandler=require('./routeHandler/todoHandler')
+const express = require("express");
+const mongoose = require("mongoose");
+const todoHandler = require("./routeHandler/todoHandler");
 
+const app = express();
+const PORT = 4000;
 
-const app=express()
-const PORT=4000;
-
-
-app.use(express.json())
-
+app.use(express.json());
 
 //database connection with mongoose
- 
-mongoose.connect('mongodb://localhost/todos')
-.then(()=> console.log('Database connection  successful'))
-.catch(err => console.log(err))
 
-   
+mongoose
+  .connect("mongodb://localhost/todos")
+  .then(() => console.log("Database connection  successful"))
+  .catch((err) => console.log(err));
 
 //application routes
-app.use('/api/v1/todo',todoHandler)
+app.use("/api/v1/todo", todoHandler);
 
 //deafult error handlers
 
-function errorHandler(err,req,res,next){
-    if(res.headersSent){
-        return next(err)
-    }
-    res.status(500).json({error:err})
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
 }
 
-app.listen(PORT,()=>{
-    console.log(`listening on PORT ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
+
+
+
+
--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,55 +1,72 @@
-const express= require('express');
-const mongoose=require('mongoose')
-const router=express.Router();
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
 
-const todoSchema=require('../schemas/todoSchema')
-const Todo=new mongoose.model('Todo',todoSchema)
-
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 //get all the todos
-router.get('/',async(req,res)=> {
-
-})
+router.get("/", async (req, res) => {});
 
 //post a todo
-router.post('/',async(req,res)=> {
-  try{
-    const newTodo=new Todo(req.body)
-    const data= await newTodo.save()
-     res.status(200).json({
-        result: data,
-        message:"Todo is inserted successfully"
+router.post("/", async (req, res) => {
+  try {
+    const newTodo = new Todo(req.body);
+    const data = await newTodo.save();
+    res.status(200).json({
+      result: data,
+      message: "Todo is inserted successfully",
     });
-} catch(err){
-     res.status(500).json({
-        error: err.message})
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
   }
- })
-
+});
 
 //get a todo by ID
-router.get('/:id',async(req,res)=> {
-
-})
-
-
+router.get("/:id", async (req, res) => {});
 
 //post multiple todos
-
-router.post('/all',async(req,res)=>{
-  await Todo.insertMany(req.body,)
-})
+router.post("/all", async (req, res) => {
+  try {
+    await Todo.insertMany(req.body);
+    res.status(200).json({
+      message: "Todos are inserted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 //update  a todo by ID
 
-router.put(':id',async(req,res)=>{
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, description, status, date } = req.body || {};
 
-})
+    const updatedData = await Todo.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          title,
+          description,
+          status,
+          date,
+        },
+      },
+      {new: true}   // for getting the new document
+    );
+    console.log(updatedData);
+    res.status(200).json({ message: "Todo is updated successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 //delete a todo by ID
 
-router.delete('/:id',async(req,res)=>{
+router.delete("/:id", async (req, res) => {});
 
-})
-
-module.exports=router;
+module.exports = router;
