0724c3b402f8fe3fcb2ba5daa756c9cdee278a14
abedinforhane <farhanengn@gmail.comm>
2022-08-05 19:21:19 +0600

    edited project name

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-
 //schemas
 const todoSchema = require("../schemas/todoSchema");
 const Todo = new mongoose.model("Todo", todoSchema);
