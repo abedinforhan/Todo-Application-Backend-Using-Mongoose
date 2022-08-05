b46ab14420fbcc0916520c05a7bfa05fffca1e08
abedinforhane <farhanengn@gmail.comm>
2022-08-05 18:43:37 +0600

    delete one document functionality completed

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -88,6 +88,21 @@ router.put("/:id", async (req, res) => {
 
 //delete a todo by ID
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+
+    try{
+        const id=req.params.id;
+        const data= await Todo.findOneAndDelete({_id:id})
+        res.status(200).json({
+            data: data,
+            message:"deleted successfully"
+        })
+
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+  
+
+});
 
 module.exports = router;
